73ddbdc5b5047583bb5b108985c442f8de844f2c
Mani Abdolalizade <mani.abdolalizade@gmail.com>
2024-03-08 12:16:57 +0330

    Edit the details of the code

--- src/UserPackage/User.java
+++ src/UserPackage/User.java
@@ -3,14 +3,9 @@ import TaskPackage.Task;
 import java.util.Scanner;
 public class User {
     Scanner Scan = new Scanner(System.in);
-    private String username;
-    private String password;
+    private String password ,username;
     public String first_name,last_name,email;
-public int streak;
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    public int streak;
     public void setFirst_name(String first_name) {
         this.first_name = first_name;
     }
@@ -23,17 +18,24 @@ public int streak;
     public void setPassword(String password) {
         this.password = password;
     }
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
     public String getFullName(){
         return  first_name + last_name;
     }
     public Task createTask(){
          String nametask = Scan.next();
-Task task1 = new Task(nametask);
-return task1;
+Task AP = new Task(nametask);
+return AP;
     }
-    /*public String getUsername(){
-
-    }*/
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
     public User (String Nusername, String Npassword, String Nfirst_name, String Nlast_name,String Nemail){
     setUsername(Nusername);
     setPassword(Npassword);
@@ -41,19 +43,11 @@ return task1;
     setLast_name(Nlast_name);
     setEmail(Nemail);
 }
-
     public void printUserInfo() {
         System.out.println("username :"+username);
         System.out.println("password :"+password);
         System.out.println("first name :"+first_name);
         System.out.println("last name :"+last_name);
         System.out.println("email :"+email);
-    }
-   public String getUsername(){
-        return username;
-   }
-
-    public String getPassword(){
-        return password;
     }
 }
